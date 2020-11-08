fd57bddda2d01dbe11f2194cd36326fd9aed5cfb
Gregory Reyes <greyes3@udi.edu.co>
2020-11-08 18:52:53 -0500

    Express Router calificación

--- routes/leaders.js
+++ routes/leaders.js
@@ -26,4 +26,26 @@ leaders.route('/')
     res.end('Deleting all the leaders!');
 });
 
+
+leaders.route('/:leaderId')
+.all((req,res,next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req,res,next) => {
+    res.end('Will send all the leaders to you!' + req.params.leaderId);
+})
+.post((req, res, next) => {
+    res.end("Post operation not supported on /leaders/"+req.params.leaderId);
+})
+.put((req, res, next) => {
+    res.statusCode = 403;
+    res.end('updating the leader:'+req.params.leaderId+"\n will update the leader:" + req.body.name + ' with details: ' + req.body.description);
+})
+.delete((req, res, next) => {
+    res.end('Deleting the leader: '+req.params.leaderId);
+});
+
+
 module.exports = leaders;
--- routes/promotions.js
+++ routes/promotions.js
@@ -26,4 +26,26 @@ promotions.route('/')
     res.end('Deleting all the promotions!');
 });
 
+
+promotions.route('/:promoId')
+.all((req,res,next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req,res,next) => {
+    res.end('Will send all the promotions to you!' + req.params.promoId);
+})
+.post((req, res, next) => {
+    res.end("Post operation not supported on /promotions/"+req.params.promoId);
+})
+.put((req, res, next) => {
+    res.statusCode = 403;
+    res.end('updating the promotions:'+req.params.promoId+"\n will update the promotions:" + req.body.name + ' with details: ' + req.body.description);
+})
+.delete((req, res, next) => {
+    res.end('Deleting the promotion: '+req.params.promoId);
+});
+
+
 module.exports = promotions;
